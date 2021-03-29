6f5b8939c5caca32d55abd2463ca7d04eb5a8888
zbsilent <zbsilent@gmail.com>
2021-03-29 19:15:30 +0800

    增加新内容

---
+++ src/components/Home/Message/Detail/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import qs from 'querystring'
+
+const Detail = (props) => {
+    console.log(props);
+    //?id=03&title=消息3
+    const xx = [{ 'id': '01', 'content': 'hello vue' },
+    { 'id': '02', 'content': 'hello react' },
+    { 'id': '03', 'content': 'hello node' }
+    ]
+    // 接受params参数
+    // const { id, title } = props.match.params;
+    const {search} = props.location;
+
+    const {id,title} = qs.parse(search).slice(1);
+    
+
+    const res =  xx.find((detailObj)=>{
+        return detailObj.id === id;
+    })
+
+  
+    return (
+        <ul>
+            <li>ID:{id}</li>
+            <li>Title:{title}</li>
+            <li>Content:{res.content}</li>
+        </ul>
+    )
+}
+export default Detail;
